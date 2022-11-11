40d6691b0c21da954fc5854c60465f309cf0b195
Matias Vera <matias@bajacloud.io>
2022-11-11 13:22:59 -0500

    feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Digital Native
+          Hello Digital Native, feature branch
         </a>
       </header>
     </div>
